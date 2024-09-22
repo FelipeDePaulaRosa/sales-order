1b82d9fa75bd3485812ec1fdddba0fb99df09e3b
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2024-09-22 19:16:56 -0300

    feat: Added product seeds

---
+++ database/seeds/Products.sql
@@ -0,0 +1,7 @@
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'f07fe56b-cfb7-4fd3-bb2c-093e18f0c3b2', N'00000006', N'Capacete', N'Carbon', 0, 13500, 0.00, 0);
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'bfb77a04-6e9e-4da3-b138-112d9e9fe46c', N'00000002', N'Martelo', N'Hammer', 1, 9400, 3.00, 122);
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'53f0ab88-4756-462c-b05f-1d150d0cd4f7', N'00000005', N'Caneta', N'Bic', 1, 300, 0.00, 33);
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'ab2152ec-e2e9-4f22-9ef4-2a03896146a8', N'00000001', N'Cutelo', N'Tramonitina', 1, 7500, 10.00, 175);
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'631cdd5d-b087-4c12-83ce-5497379a4879', N'00000004', N'Saladeira', N'Wolf', 1, 4500, 0.00, 64);
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'71f7723f-f4dd-4696-a901-d92eb2bc4817', N'00000003', N'Garrafa', N'Stanley', 1, 27500, 25.00, 82);
+INSERT INTO master.dbo.Product (Id, Code, Name, Brand, Active, UnitPrice, Discount, Stock) VALUES (N'192f128a-34a8-4f1e-9c11-ee62561a4bb2', N'00000007', N'Vaso', N'A Casa', 1, 5000, 0.00, 0);
