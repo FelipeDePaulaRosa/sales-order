982545df2c485c0651e73471509e85fabe73aa67
felipe.rosa <felipe.rosa@tinnova.com.br>
2024-09-24 22:49:17 -0300

    test: Added more unit tests cases for UpdateOrderHandler

--- src/UnitTest/Application/Orders/UpdateOrder/UpdateOrderHandlerTest.cs
+++ src/UnitTest/Application/Orders/UpdateOrder/UpdateOrderHandlerTest.cs
@@ -61,4 +61,151 @@ public class UpdateOrderHandlerTest
             updatedOrder.Products.Should().Contain(p => p.ProductId == product.ProductId);
         }
     }
+    
+    [Fact]
+    public async Task Should_AddStockEvent_When_ProductIsCanceled()
+    {
+        var products = ProductFaker.GenerateSpecificCountOfProducts(1);
+        var order = OrderFaker.GenerateOrderAsCreated(Guid.NewGuid(), products);
+        order = await _orderRepository.CreateAsync(order);
+
+        var productRequest = new UpdateOrderProductRequest
+        {
+            Id = order.Products.First().Id,
+            ProductId = order.Products.First().Id,
+            Quantity = order.Products.First().Quantity,
+            IsCanceled = true
+        };
+
+        var request = new UpdateOrderRequest
+        {
+            Id = order.Id,
+            Products = new List<UpdateOrderProductRequest> { productRequest }
+        };
+
+        await _handler.Handle(request, CancellationToken.None);
+
+        var updatedOrder = await _orderRepository.GetOrderByIdOrDefaultNoTrackAsync(order.Id);
+        var updatedProduct = updatedOrder.Products.First();
+        updatedProduct.IsCanceled.Should().BeTrue();
+    }
+    
+    [Fact]
+    public async Task Should_RemoveStockEvent_When_ProductIsUncanceled()
+    {
+        var products = ProductFaker.GenerateSpecificCountOfProducts(1);
+        var order = OrderFaker.GenerateOrderAsCreated(Guid.NewGuid(), products);
+        var product = order.Products.First();
+        order.UpdateProduct(order.Products.First().Id, product.Quantity, true);
+        await _orderRepository.CreateAsync(order);
+
+        var productRequest = new UpdateOrderProductRequest
+        {
+            Id = order.Products.First().Id,
+            ProductId = product.Id,
+            Quantity = product.Quantity,
+            IsCanceled = false
+        };
+
+        var request = new UpdateOrderRequest
+        {
+            Id = order.Id,
+            Products = new List<UpdateOrderProductRequest> { productRequest }
+        };
+
+        await _handler.Handle(request, CancellationToken.None);
+
+        var updatedOrder = await _orderRepository.GetOrderByIdOrDefaultNoTrackAsync(order.Id);
+        var updatedProduct = updatedOrder.Products.First();
+        updatedProduct.IsCanceled.Should().BeFalse();
+    }
+    
+    [Fact]
+    public async Task Should_RemoveStockEvent_When_QuantityIncreases()
+    {
+        var products = ProductFaker.GenerateSpecificCountOfProducts(1);
+        var order = OrderFaker.GenerateOrderAsCreated(Guid.NewGuid(), products);
+        await _orderRepository.CreateAsync(order);
+    
+        var product = order.Products.First();
+        var newQuantity = product.Quantity + 5;
+        var productRequest = new UpdateOrderProductRequest
+        {
+            Id = order.Products.First().Id,
+            ProductId = product.Id,
+            Quantity = newQuantity,
+            IsCanceled = false
+        };
+    
+        var request = new UpdateOrderRequest
+        {
+            Id = order.Id,
+            Products = new List<UpdateOrderProductRequest> { productRequest }
+        };
+    
+        await _handler.Handle(request, CancellationToken.None);
+    
+        var updatedOrder = await _orderRepository.GetOrderByIdOrDefaultNoTrackAsync(order.Id);
+        var updatedProduct = updatedOrder.Products.First();
+        updatedProduct.Quantity.Should().Be(newQuantity);
+    }
+    
+    [Fact]
+    public async Task Should_AddStockEvent_When_QuantityDecreases()
+    {
+        var products = ProductFaker.GenerateSpecificCountOfProducts(1);
+        var order = OrderFaker.GenerateOrderAsCreated(Guid.NewGuid(), products);
+        await _orderRepository.CreateAsync(order);
+    
+        var product = order.Products.First();
+        var newQuantity = product.Quantity - 5;
+        var productRequest = new UpdateOrderProductRequest
+        {
+            Id = order.Products.First().Id,
+            ProductId = product.Id,
+            Quantity = newQuantity,
+            IsCanceled = false
+        };
+    
+        var request = new UpdateOrderRequest
+        {
+            Id = order.Id,
+            Products = new List<UpdateOrderProductRequest> { productRequest }
+        };
+    
+        await _handler.Handle(request, CancellationToken.None);
+    
+        var updatedOrder = await _orderRepository.GetOrderByIdOrDefaultNoTrackAsync(order.Id);
+        var updatedProduct = updatedOrder.Products.First();
+        updatedProduct.Quantity.Should().Be(newQuantity);
+    }
+    
+    [Fact]
+    public async Task Should_NotTriggerEvents_When_QuantityIsSame()
+    {
+        var products = ProductFaker.GenerateSpecificCountOfProducts(1);
+        var order = OrderFaker.GenerateOrderAsCreated(Guid.NewGuid(), products);
+        await _orderRepository.CreateAsync(order);
+    
+        var product = order.Products.First();
+        var productRequest = new UpdateOrderProductRequest
+        {
+            Id = order.Products.First().Id,
+            ProductId = product.Id,
+            Quantity = product.Quantity,
+            IsCanceled = false
+        };
+    
+        var request = new UpdateOrderRequest
+        {
+            Id = order.Id,
+            Products = new List<UpdateOrderProductRequest> { productRequest }
+        };
+    
+        await _handler.Handle(request, CancellationToken.None);
+    
+        var updatedOrder = await _orderRepository.GetOrderByIdOrDefaultNoTrackAsync(order.Id);
+        var updatedProduct = updatedOrder.Products.First();
+        updatedProduct.Quantity.Should().Be(product.Quantity);
+    }
 }
