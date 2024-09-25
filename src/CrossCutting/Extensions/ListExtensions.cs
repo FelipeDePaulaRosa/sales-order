1fdf9d01d289372eccfbfcb39fd88e7b70a84aac
felipe.rosa <felipe.rosa@tinnova.com.br>
2024-09-25 11:48:23 -0300

    feat: Created objects to paginate a list

---
+++ src/CrossCutting/Extensions/ListExtensions.cs
@@ -0,0 +1,24 @@
+﻿using CrossCutting.Utils;
+
+namespace CrossCutting.Extensions;
+
+public static class ListExtensions
+{
+    public static PagedListResponse<T> ToPaged<T>(this List<T> source, int pageNumber, int pageSize)
+    {
+        var items = source
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+
+        var pagedList = new PagedList<T>(items, source.Count, pageNumber, pageSize);
+
+        return new PagedListResponse<T>(
+            pagedList.CurrentPage,
+            pagedList.PageSize,
+            pagedList.TotalPages,
+            pagedList.TotalContent,
+            pagedList
+        );
+    }
+}
